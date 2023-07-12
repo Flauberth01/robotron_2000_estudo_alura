76f5d630824ac974fd72c67d26f8606b8c432202
Flauberth01 <flauberth12@gmail.com>
2023-07-12 10:23:18 -0500

    Refactor: Melhoria na lógica do código

--- js/main.js
+++ js/main.js
@@ -1,47 +1,21 @@
 const subtrair = document.querySelector("#subtrair")
 const somar = document.querySelector("#somar")
-const braco = document.querySelector("#braco")
 
 const controle = document.querySelectorAll(".controle-ajuste")
 
-controle.forEach( (elemento) => {
-    elemento.addEventListener("click", (elemento) => {
-        console.log(elemento.target)
+controle.forEach((elemento) => { 
+    console.log(elemento)
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode)
     })
 })
 
-somar.addEventListener("click", () => {manipulaDados("somar")})
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector(".controle-contador")
 
-subtrair.addEventListener("click", () => {manipulaDados("subtrair")})
-
-function manipulaDados(operacao) {
-    if(operacao === "subtrair") {
-        braco.value = parseInt(braco.value) - 1
+    if(operacao === "-") {
+        peca.value = parseInt(peca.value) - 1
     } else {
-        braco.value = parseInt(braco.value) + 1
+        peca.value = parseInt(peca.value) + 1
     }
 }
-
-// const blindagemSom =  document.querySelector('#blindagem-soma')
-// const blindagemSub =  document.querySelector('#blindagem-sub')
-// const blindagemValue =  document.querySelector('#blindagem-value')
-
-// blindagemSom.addEventListener("click", somaClick)
-// blindagemSub.addEventListener("click",  subClick)
-
-// let valor = 0
-
-// function somaClick() {
-//     valor += 1
-//     console.log(valor);
-//     blindagemValue.value = valor
-
-// }
-
-// function subClick() {
-//     if (valor > 0 ) {
-//         valor -= 1
-//         blindagemValue.value = valor
-//     }
-
-// }
